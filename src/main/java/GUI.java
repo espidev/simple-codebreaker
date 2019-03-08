2634092d4c43bcda7b10a9f8399569e09c299051
Raz <benhaim.raz@gmail.com>
2019-03-08 09:58:57 -0500

    Gui on the way!

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class GUI extends JFrame implements ActionListener{
 	//Create some panels
@@ -11,26 +12,53 @@ public class GUI extends JFrame implements ActionListener{
     JButton start = new JButton("start");
 	// CONSTRUCTOR - Setup your GUI here
     public GUI(){
+      start.setSize(500,500);
       FlowLayout layout1 = new FlowLayout();
-      GridLayout layout2 = new GridLayout(5,10);
+      GridLayout layout2 = new GridLayout(4,10); //displays the colors to the user 
+      GridLayout layout3 = new GridLayout(4, 10); //displays the feedback to the user
 	  setTitle("Code breaker");
 	  setSize(500, 400);
 	  setResizable(true);
 	  Color greyish = new Color(192, 192, 192);
-	  pan1.setBackground(greyish);
+	  //pan1.setBackground(greyish);
 	  pan2.setBackground(greyish);
 	  pan3.setBackground(greyish);
+	
 	  setLayout(layout2);//Setting layout for the whole frame
 	  pan1.setLayout(layout1);//Layout for Pan1
-	  pan1.add(start);
 	  pan1.add(welcomeLabel);
-	  pan1.setVisible(true);
+	  pan1.add(start);
+	  
+	  add(pan1);
 	  setVisible(true);
 	  //
+	  start.addActionListener((event) -> {
+		  String command = event.getActionCommand();
+	      
+	        if (command.equals("start")){
+	          start.setVisible(false);
+	          welcomeLabel.setVisible(false);
+	          //beginGame();
+	        }
+	  });
+	  
+	  
     }
+    /*
+    public void actionPerformed(ActionEvent event) {
+        String command = event.getActionCommand();
+      
+        if (command.equals("start")){
+          start+setVisible
+        }
+   }
+   */
+
     public static void main(String[] args){
     	GUI frame1 = new GUI();  //Start the GUI
     }
+	
+	
     
 }
 		
